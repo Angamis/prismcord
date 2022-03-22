d38612762842357f750e92c0115714ff9c399abb
Angamis <simonoser@outlook.com>
2022-03-22 08:26:40 +0100

    Extracted the timer to its own class Added new format for time: "EEE h:mm a", Locale.ENGLISH

--- src/main/java/prism/App.java
+++ src/main/java/prism/App.java
@@ -6,11 +6,6 @@ import org.javacord.api.entity.server.Server;
 import org.javacord.api.entity.user.User;
 import org.javacord.api.util.logging.FallbackLoggerConfiguration;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.*;
-
 public class App {
 
     public static void main(String[] args) {
@@ -26,34 +21,15 @@ public class App {
         FallbackLoggerConfiguration.setDebug(true);
 
         String token = args[0];
-
         DiscordApi api = new DiscordApiBuilder()
                 .setToken(token)
                 .login()
                 .join();
 
         Server server = api.getServerById(Long.parseLong(args[1])).get();
-
         User selfUser = api.getYourself();
 
-        TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
+        TimedRename.setUpTimer(selfUser, server);
 
-        TimerTask timerTask = new TimerTask() {
-            @Override
-            public void run() {
-                String time = new SimpleDateFormat("HH : mm").format(new Date());
-
-                selfUser.updateNickname(server, time);
-            }
-        };
-
-        Timer timer = new Timer();
-        timer.scheduleAtFixedRate(timerTask, millisToNextMinute(),  60000);
-    }
-
-    //have 5 second as buffer
-    private static long millisToNextMinute() {
-        LocalDateTime nextMinute = LocalDateTime.now().plusMinutes(1).plusSeconds(5).truncatedTo(ChronoUnit.MINUTES);
-        return LocalDateTime.now().until(nextMinute, ChronoUnit.MILLIS);
     }
 }
