60518e93d66aa345eb129d701b491a3554b604bf
Angamis <simonoser@outlook.com>
2022-03-23 11:53:34 +0100

    implemented functionality

--- src/main/java/prism/commands/ChangeTimeZoneCommand.java
+++ src/main/java/prism/commands/ChangeTimeZoneCommand.java
@@ -1,16 +1,34 @@
 package prism.commands;
 
+import org.javacord.api.DiscordApi;
+import org.javacord.api.entity.server.Server;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
+import prism.App;
+import prism.TimedBotName;
+
+import java.util.Optional;
+import java.util.TimeZone;
 
 public class ChangeTimeZoneCommand implements MessageCreateListener, CommandStructure {
 
-    private final String COMMAND_NAME = "timeZone";
-    private final String COMMAND_DESCRIPTION = "a command that changes the time zone of the bot";
+    private static final String COMMAND_NAME = "timeZone";
 
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
-        //TODO
+
+        String ownCommand = App.getPrefix() + COMMAND_NAME;
+
+        if (event.getMessageContent().substring(0, ownCommand.length()).equalsIgnoreCase(ownCommand)) {
+
+            String timeZoneName = event.getMessageContent().substring(ownCommand.length() + 1);
+            TimeZone timeZone = TimeZone.getTimeZone(timeZoneName);
+
+            DiscordApi api = event.getApi();
+            Optional<Server> serverOptional = event.getServer();
+
+            serverOptional.ifPresent(server -> TimedBotName.setUpTimer(api.getYourself(), server, timeZone));
+        }
     }
 
     @Override
@@ -20,6 +38,6 @@ public class ChangeTimeZoneCommand implements MessageCreateListener, CommandStru
 
     @Override
     public String getCommandDescription() {
-        return COMMAND_DESCRIPTION;
+        return "a command that changes the time zone of the bot, use Java time zone IDs";
     }
 }
