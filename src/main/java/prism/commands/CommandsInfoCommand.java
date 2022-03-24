7cd13fab8e460ff7d5de95e87cce63ced5acc5ce
Angamis <simonoser@outlook.com>
2022-03-24 09:18:17 +0100

    added StringBuilder to make it look nice

--- src/main/java/prism/commands/CommandsInfoCommand.java
+++ src/main/java/prism/commands/CommandsInfoCommand.java
@@ -5,6 +5,8 @@ import org.javacord.api.listener.message.MessageCreateListener;
 import prism.App;
 import prism.CommandStructure;
 
+import java.util.Map;
+
 public class CommandsInfoCommand implements MessageCreateListener, CommandStructure {
 
     private static final String COMMAND_NAME = "commands";
@@ -15,7 +17,7 @@ public class CommandsInfoCommand implements MessageCreateListener, CommandStruct
         String ownCommand = App.getPrefix() + COMMAND_NAME;
 
         if (event.getMessageContent().equalsIgnoreCase(ownCommand)) {
-            event.getChannel().sendMessage(App.getCommandsMap().toString());
+            event.getChannel().sendMessage(getDescriptionOfAllCommandsFormatted());
         }
     }
 
@@ -27,5 +29,23 @@ public class CommandsInfoCommand implements MessageCreateListener, CommandStruct
     @Override
     public String getCommandDescription() {
         return "You just used this command \uD83D\uDE03";
+    }
+
+    private String getDescriptionOfAllCommandsFormatted () {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        Map<String, String> commandsMap = App.getCommandsMap();
+
+        for (Map.Entry<String, String> entry : commandsMap.entrySet()) {
+            stringBuilder.append("> ");
+            stringBuilder.append(App.getPrefix());
+            stringBuilder.append(entry.getKey());
+            stringBuilder.append("\n");
+            stringBuilder.append("```");
+            stringBuilder.append(entry.getValue());
+            stringBuilder.append("``` \n");
+        }
+
+        return stringBuilder.toString();
     }
 }
