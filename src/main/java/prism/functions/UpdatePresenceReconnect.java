50fe914aa30fbff6bf784f7dab8b514cb824c644
Angamis <simonoser@outlook.com>
2022-03-24 09:18:39 +0100

    link to static String from App.java

--- src/main/java/prism/functions/UpdatePresenceReconnect.java
+++ src/main/java/prism/functions/UpdatePresenceReconnect.java
@@ -4,12 +4,13 @@ import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.activity.ActivityType;
 import org.javacord.api.event.connection.ReconnectEvent;
 import org.javacord.api.listener.connection.ReconnectListener;
+import prism.App;
 
 public class UpdatePresenceReconnect implements ReconnectListener {
 
     @Override
     public void onReconnect(ReconnectEvent event) {
         DiscordApi api = event.getApi();
-        api.updateActivity(ActivityType.WATCHING, "FFXIV Server Time");
+        api.updateActivity(ActivityType.WATCHING, App.getPresenceString());
     }
 }
